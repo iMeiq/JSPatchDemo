80c4be22e57283b3fb9bc726dc4bc14a312dd4cb
meiqing <meiqing@meiqingdemacbook-pro.local>
2015-12-12 18:23:21 +0800

    Initial Commit

---
+++ JsPatchDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JsPatchDemo
+//
+//  Created by meiqing on 15/12/12.
+//  Copyright © 2015年 meiqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JsPatchDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JsPatchDemo
+//
+//  Created by meiqing on 15/12/12.
+//  Copyright © 2015年 meiqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
